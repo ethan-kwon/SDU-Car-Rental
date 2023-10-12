42422cf3febecd9d769d92ac643e83ebeeebdd35
Mateusz Poczta <mapoc23@student.sdu.dk>
2023-10-12 16:38:53 +0200

    created new main directory src

--- car-rental/App.tsx
+++ car-rental/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import {StyleSheet} from 'react-native';
-import RootNavigator from "./navigation/RootNavigator";
+import RootNavigator from "./src/navigation/RootNavigator";
 
 export default function App() {
     return (
