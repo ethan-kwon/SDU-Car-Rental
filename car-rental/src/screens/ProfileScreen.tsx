3b91d9bd91f838adbb2e88054fbd314fa5dd416d
kiyue23 <144211570+kiyue23@users.noreply.github.com>
2024-01-09 03:55:43 +0800

    Delete car-rental directory

--- car-rental/src/screens/ProfileScreen.tsx
+++
@@ -1,9 +0,0 @@
-import {Text} from "react-native";
-
-const ProfileScreen = () => {
-    return (
-        <Text>This is a profile screen!</Text>
-    );
-}
-
-export default ProfileScreen;
--- car-rental/src/styles/colors.ts
+++
@@ -1,8 +0,0 @@
-export const COLORS = {
-    WHITE: '#FCFCFC',
-    BLACK: '#000',
-    GREEN: '#0E782E',
-    DARK_GRAY: '#404040',
-    LIGHT_GRAY: '#7C7C7C',
-    LIGHT_LIGHT_GRAY: '#F5F5F5'
-}
